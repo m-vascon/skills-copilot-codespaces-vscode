7983cb948b2a2c16ec039f3decd34353c57de78d
M Vascon <123307180+m-vascon@users.noreply.github.com>
2025-02-09 13:59:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,28 @@
+function skillsMember() {
+  return [
+    {
+      title: "HTML",
+      icon: "html5",
+    },
+    {
+      title: "CSS",
+      icon: "css3",
+    },
+    {
+      title: "JavaScript",
+      icon: "js",
+    },
+    {
+      title: "React",
+      icon: "react",
+    },
+    {
+      title: "Node.js",
+      icon: "node",
+    },
+    {
+      title: "MongoDB",
+      icon: "mongodb",
+    },
+  ];
+}
